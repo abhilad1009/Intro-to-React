6d22ed7fc21af1b647fc313b2ecbe0a8c5348615
Abhi Lad <42882493+abhiwolf13@users.noreply.github.com>
2021-05-29 23:01:54 +0530

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -4,27 +4,49 @@ import ReactDom from "react-dom";
 // CSS
 import "./index.css";
 // Variables
-const author = "John Doe";
-const title = "Book Title";
-const img =
-  "https://upload.wikimedia.org/wikipedia/commons/thumb/9/94/Bamboo_book_-_binding_-_UCR.jpg/255px-Bamboo_book_-_binding_-_UCR.jpg";
+// const author = "John Doe";
+// const title = "Book Title";
+// const img =
+//   "https://upload.wikimedia.org/wikipedia/commons/thumb/9/94/Bamboo_book_-_binding_-_UCR.jpg/255px-Bamboo_book_-_binding_-_UCR.jpg";
+
+const books = [
+  {
+    id: 1,
+    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/94/Bamboo_book_-_binding_-_UCR.jpg/255px-Bamboo_book_-_binding_-_UCR.jpg",
+    title: "Book Title",
+    author: "John Doe",
+  },
+  {
+    id: 2,
+    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/94/Bamboo_book_-_binding_-_UCR.jpg/255px-Bamboo_book_-_binding_-_UCR.jpg",
+    title: "Book2 Title",
+    author: "Jane Doe",
+  },
+];
 function BookList() {
   return (
     <section className="booklist">
-      <Book job="developer" />
-      <Book />
+      {books.map(function (book) {
+        return (
+          <Book
+            key={book.id}
+            img={book.img}
+            title={book.title}
+            author={book.author}
+          ></Book>
+        );
+      })}
     </section>
   );
 }
 
-function Book(props) {
-  console.log(props);
+function Book(props, { children }) {
+  // console.log(props);
   return (
     <article className="book">
-      <img src={img} alt="" />
-      <h1>{title}</h1>
-      <h4>{author}</h4>
-      <p>{props.job}</p>
+      <img src={props.img} alt="" />
+      <h1>{props.title}</h1>
+      <h4>{props.author}</h4>
     </article>
   );
 }
